314533a6bc49de7aa67596777b4ec82ece65bce4
KnJbMfLAgdkwZL <zipocat@gmail.com>
2024-03-09 12:51:44 +0200

    Update MapExanimaC.cpp
    Error fix

--- MapExanimaC.cpp
+++ MapExanimaC.cpp
@@ -16,6 +16,8 @@
 name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
 processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
+#pragma comment(lib, "d2d1.lib")
+
 using namespace std;
 
 #define MAX_LOADSTRING 100
